7295c212cdfeef44d6b288a70d2ef468cb6b43a0
shulu <961085397@qq.com>
2021-05-18 14:53:50 +0800

    finish QuestionBox

--- demo2/src/components/Header.vue
+++ demo2/src/components/Header.vue
@@ -2,7 +2,15 @@
   <div>
     <b-nav>
       <b-nav-item disabled><b>Fancy QuestionBox App</b></b-nav-item>
-      <b-nav-item disabled>Count: 4/10</b-nav-item>
+      <b-nav-item disabled>Count: {{numCorrect}}/{{numTotal}}</b-nav-item>
     </b-nav>
   </div>
 </template>
+<script>
+export default {
+  props: [
+    'numCorrect',
+    'numTotal'
+  ],
+}
+</script>
--- demo2/src/components/QuestionBox.vue
+++ demo2/src/components/QuestionBox.vue
@@ -6,24 +6,29 @@
       </template>
       <hr class="my-4">
       <b-list-group>
-        <b-list-group-item v-for="(answer, index) in answers" :key="index" @click="selectAnswer(index)" :class="[selectedIndex === index ? 'selected' :'']">
+        <b-list-group-item v-for="(answer, index) in answers" :key="index" @click="selectAnswer(index)" :class="answerClass(index)">
           {{answer}}
         </b-list-group-item>
       </b-list-group>
-      <b-button variant="primary" href="#">Submit </b-button>
+      <b-button variant="primary" @click="submitAnswer" :disabled="selectedIndex === null || answered">Submit </b-button>
       <b-button @click="next" variant="success" href="#">Next</b-button>
     </b-jumbotron>
   </div>
 </template>
 <script>
+import _ from 'lodash'
 export default {
   props: {
     currentQuestion: Object,
-    next: Function
+    next: Function,
+    increment: Function
   },
   data () {
     return {
-      selectedIndex: null
+      selectedIndex: null,
+      correctIndex: null,
+      shuffledAnswers: [],
+      answered: false
     }
   },
   computed: {
@@ -33,24 +38,44 @@ export default {
       return answers
     }
   },
-  watched: {
-    currentQuestion () {
-      this.selectedIndex = null
-      this.shuffleAnswers()
-    },
+  watch: {
+    currentQuestion: {
+      immediate: true,
+      handler () {
+        this.selectedIndex = null
+        this.answered = null
+        this.shuffleAnswers()
+      }
+    }
   },
   methods: {
     selectAnswer (index) {
       this.selectedIndex = index
     },
+    submitAnswer () {
+      let isCorrect = false
+      if (this.selectedIndex === this.correctIndex) {
+        isCorrect = true
+      }
+      this.answered = true
+      this.increment(isCorrect)
+    },
     shuffleAnswers () {
-      let answers = [...this.currentQuestion.incorrect_answers]
-      answers.push(this.currentQuestion.correct_answer)
-      return answers
+      let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer]
+      this.shuffledAnswers = _.shuffle(answers)
+      this.correctIndex = this.shuffledAnswers.indexOf(this.currentQuestion.correct_answer)
+    },
+    answerClass (index) {
+      let answerClass = ''
+      if (!this.answered && this.selectedIndex === index) {
+        answerClass = 'selected'
+      } else if (this.answered && this.correctIndex === index) {
+        answerClass = 'correct'
+      } else if (this.answered && this.selectedIndex === index && this.correctIndex !== index) {
+        answerClass = 'incorrect'
+      }
+      return answerClass
     }
-  },
-  mounted () {
-    console.log(this.currentQuestion)
   }
 }
 </script>
