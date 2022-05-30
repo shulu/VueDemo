d0de49375e1e86705b1ef6f26ae3a17a8919c39f
sarcasme <961085397@qq.com>
2022-05-30 23:20:14 +0800

    windows好烦
    windows好烦

--- vue-feed-app/src/views/UserInfo.vue
+++ vue-feed-app/src/views/UserInfo.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="show-qr">
+  <div class="user-info-setting">
     <top-modal title="用户设置" />
-    <div class="user-info">
+    <div class="user-info-content">
       <div class="user-info-set">
         <div>set avatar</div>
         <div>nickname</div>
@@ -13,9 +13,9 @@
         <div>email</div>
         <div>phone</div>
       </div>
+      <div class="qrcode">qrcode</div>
+      <div>退出登录</div>
     </div>
-    <div class="qrcode">qrcode</div>
-    <div>退出登录</div>
   </div>
 </template>
 
@@ -23,112 +23,30 @@
 import TopModal from "@/components/TopModal";
 
 export default {
-  name: "ShowQR",
+  name: "UserInfo",
   components: {
     TopModal,
   },
   data() {
-    return {
-      qrLink: "",
-    };
+    return {};
   },
-  created() {
-    this.initQR();
-  },
-  methods: {
-    initQR() {
-      let nowTime = this.getTimeStamp();
-      this.qrLink =
-        "https://api.qrserver.com/v1/create-qr-code/?size=300x300&color=006400&data=" +
-        nowTime;
-    },
-    refreshQR() {
-      let nowTime = this.getTimeStamp();
-      this.qrLink =
-        "https://api.qrserver.com/v1/create-qr-code/?size=300x300&color=006400&data=" +
-        nowTime;
-    },
-    getTimeStamp() {
-      var date = Date.parse(new Date());
-      date = date / 1000;
-      return date;
-    },
-  },
+  created() {},
+  methods: {},
   computed: {},
 };
 </script>
 
 <style scoped lang="scss">
-.show-qr {
+.user-info-setting {
   height: 100%;
-  padding: 0 2em;
-
-  .qr-content {
-    z-index: 9;
+  .user-info-content {
     margin: auto;
     position: absolute;
-    top: -4em;
+    text-align: center;
+    top: 0;
     left: 0;
     right: 0;
     bottom: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    gap: 0.5em;
-
-    .avatar {
-      img {
-        width: 6em;
-        height: 6em;
-        border-radius: 3em;
-      }
-    }
-
-    .uid {
-      font-size: 1.3em;
-      font-weight: 800;
-      color: #ff7f50;
-    }
-
-    .username {
-      font-size: 0.85em;
-    }
-
-    .qrcode {
-      margin: 15px 0;
-      img {
-        width: 12em;
-        height: 12em;
-      }
-    }
-
-    .refresh-qr {
-      height: 2em;
-      img {
-        display: inline;
-        float: left;
-        width: 1em;
-        height: 1em;
-        margin: 0.3em 0.3em;
-      }
-      p {
-        display: inline;
-        font-size: 0.9em;
-        color: gray;
-        font-weight: 900;
-        opacity: 0.6;
-        line-height: 2em;
-      }
-    }
-
-    .tips-f,
-    .tips-s {
-      font-size: 0.1em;
-      color: gray;
-      font-weight: 500;
-      opacity: 0.6;
-    }
   }
 }
 </style>
